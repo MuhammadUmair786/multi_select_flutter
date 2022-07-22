1b66d0cbc08f3eba0b40767f90e5b426d8175bf1
Muhammad Umair Bin Sajid <64140593+MuhammadUmair786@users.noreply.github.com>
2022-07-23 00:09:06 +0500

    Revert "add listtile with img avatar"
    This reverts commit 576f0209410b9a145d988242b1873ddea8b59a7e.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -67,8 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
     Animal(id: 27, name: "Dolphin"),
   ];
   final _items = _animals
-      .map((animal) =>
-          MultiSelectItem<Animal>(value: animal, label: animal.name))
+      .map((animal) => MultiSelectItem<Animal>(animal, animal.name))
       .toList();
   //List<Animal> _selectedAnimals = [];
   List<Animal> _selectedAnimals2 = [];
--- lib/util/multi_select_item.dart
+++ lib/util/multi_select_item.dart
@@ -2,10 +2,7 @@
 class MultiSelectItem<T> {
   final T value;
   final String label;
-  final String? subTitle;
-  final String? imgUrl;
   bool selected = false;
 
-  MultiSelectItem(
-      {required this.value, required this.label, this.subTitle, this.imgUrl});
+  MultiSelectItem(this.value, this.label);
 }
